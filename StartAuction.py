b9a668232b3abf6dc7caa55119d49210516597dd
localhost-server <99524394+localhost-server@users.noreply.github.com>
2024-08-19 12:35:20 +0530

    Update StartAuction.py

--- StartAuction.py
+++ StartAuction.py
@@ -67,7 +67,7 @@ async def scrape_auction_data(auction_link, collection, link_collection):
             if not data:
                 return
 
-            data_list = [{"carLink": k, "price": v , "date": datetime.now(cdt).date().strftime("%d-%m-%Y").replace('-','.')} for k, v in data.items() if k != 'None' and v != ""]
+            data_list = [{"carLink": k, "price": int(v.replace("$",'').replace(",",'')) , "date": datetime.now(cdt).date().strftime("%d-%m-%Y").replace('-','.')} for k, v in data.items() if ("https://www.iaai.com/" in k) and (v != "")]
             carLink_list = [i['carLink'] for i in data_list]
 
             subprocess.Popen(["python3", "check_link.py", ' '.join(carLink_list)])
