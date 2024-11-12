0ba300c7b897ad07a89010a44294fe00179ce4e2
localhost-server <99524394+localhost-server@users.noreply.github.com>
2024-11-12 22:08:18 +0530

    Update ProductScraping.py

--- ProductScraping.py
+++ ProductScraping.py
@@ -64,7 +64,7 @@ async def main():
     #         "password": passwd
     #         }) #{"server": "socks5://127.0.0.1:9051"})
     context = browser
-    page =  await context.new_page()
+    page = browser.pages[0] #await context.new_page()
     # Enabling the extension for incognito mode
     # await page.goto(f"chrome://extensions/?id={extension_id}")
     # await asyncio.sleep(3)
